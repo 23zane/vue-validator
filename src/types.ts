133998abc1fca7e496b3f8e23a9989fbf8b0aa69
dzanetti <dennis.zanetti@wecanconsulting.it>
2021-03-04 12:50:50 +0100

    Function callbacks

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export type GenericInput = {
 	mandatory: boolean;
 	name: string;
 	exclude_dirty?: boolean; // Per evitare il controllo dirty
-	rules: (string | {key: string, func: (value: string | string[] | number[]) => boolean})[];
+	rules: (string | {key: string, func: <K extends string | string[] | number[] | boolean = string | string[] | number[] | boolean>(value: K) => boolean})[];
 };
 
 export type InputType<E, I extends GenericInput = GenericInput> = Record<keyof E, I>;
